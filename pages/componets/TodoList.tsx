ba395b58466c6e3603c4ca5cea048e903630a0b5
Naoya Noguchi <naoya.work0213@gmail.com>
2025-05-09 07:48:48 +0900

    TodoList更新

--- pages/componets/TodoList.tsx
+++ pages/componets/TodoList.tsx
@@ -4,9 +4,19 @@ const TodoList = () => {
   return (
     <div>
       <ul className="mx-auto">
-        <div className="flex bg-orange-200 rounded-md">
-          <li>✅　読書</li>
-          <span>✖︎</span>
+        <div className="flex bg-orange-200 rounded-md mt-2 p-2 justify-between">
+          <li className="font-medium">✅　読書</li>
+          <span className="cursor-pointer">✖︎</span>
+        </div>
+
+        <div className="flex bg-orange-200 rounded-md mt-2 p-2 justify-between">
+          <li className="font-medium">✅　散歩</li>
+          <span className="cursor-pointer">✖︎</span>
+        </div>
+
+        <div className="flex bg-orange-200 rounded-md mt-2 p-2 justify-between">
+          <li className="font-medium">✅　プログラミング</li>
+          <span className="cursor-pointer">✖︎</span>
         </div>
       </ul>
     </div>
